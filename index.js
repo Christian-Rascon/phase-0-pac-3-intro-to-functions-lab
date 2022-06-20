4491e17437f009062a007c7bd7b637da82e48fa4
Christian <christianrascon21@icloud.com>
2022-06-20 09:29:08 -0600

    commit tree c6312238ed64d75b841f4c80874f42cc1e60e032

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(hello) {
+    console.log (hello.toUpperCase());
+}
+function logWhisper(HELLO) {
+    console.log (HELLO.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(hello) {
+    console.log (hello.toUpperCase());
+    return 
+}
